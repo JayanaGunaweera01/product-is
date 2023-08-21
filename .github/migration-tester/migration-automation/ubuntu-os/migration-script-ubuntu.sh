94c3e0f09b7ca5e7bee86aa11b6dec4152f281b0
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-08-21 22:25:43 +0530

    Update migration-script-ubuntu.sh

--- .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -93,7 +93,7 @@ cd "$AUTOMATION_HOME"
 
 # Download needed wso2IS zip
 chmod +x download-GA-pack.sh
-bash download-GA-pack.sh "$keyJsonFile"
+bash download-GA-pack.sh "$keyJsonFile" "$currentVersion"
 
 #cd "$AUTOMATION_HOME"
 cd "$UBUNTU_HOME"
