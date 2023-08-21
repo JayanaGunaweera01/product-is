0778a7cb0c6d86e8a90ae9bfc8a59691226162d1
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-08-21 17:37:35 +0530

    Update download-GA-pack.sh

--- .github/migration-tester/migration-automation/download-GA-pack.sh
+++ .github/migration-tester/migration-automation/download-GA-pack.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
+# Define color variables
+GREEN='\033[0;32m\033[1m' # green color
+RED='\033[0;31m\033[1m'   # red color
+NC='\033[0m'           # reset color
+
+# Get the value of the inputs
+os=$1
+currentVersion=$3
+
+# Source the env.sh file
+chmod +x ./env.sh
+./env.sh
+echo -e "${GREEN}==> Env file for $os sourced successfully${RESET}"
+
+
+# Initialize file_id variable
+file_id=""
+
+# Check the value of currentVersion and assign the corresponding environment variable to file_id
+case $currentVersion in
+  5.9.0)
+    file_id="$FILE_ID_5_9"
+    ;;
+  5.10.0)
+    file_id="$FILE_ID_5_10"
+    ;;
+  5.11.0)
+    file_id="$FILE_ID_5_11"
+    ;;
+  6.0.0)
+    file_id="$FILE_ID_6_0"
+    ;;
+  6.1.0)
+    file_id="$FILE_ID_6_1"
+    ;;
+  *)
+    echo "No action taken.Please assign a value in env.sh if you haven't assigned a value for file ID."
+    ;;
+esac
+
+# Use the file_id variable in downloading the IS zip
+echo "file_id: $file_id"
+
+# Specify the Google Drive file URL
+file_url="https://www.googleapis.com/drive/v3/files/15nZ0gwIo-4YMibykGD979BG_olw5ChgV?alt=media"
+
+pwd
+ls -a
+# Download the file using the access token
 set -euo pipefail
 
 base64var() {
@@ -10,7 +59,7 @@ base64stream() {
     base64 | tr '/+' '_-' | tr -d '=\n'
 }
 
-keyJsonFile=$1
+keyJsonFile=$2
 scope="https://www.googleapis.com/auth/drive.readonly"
 valid_for_sec="${3:-3600}"
 valid_for_sec_numeric=$(echo "$valid_for_sec" | tr -d -c '[:digit:]')
@@ -58,16 +107,15 @@ response=$(curl -v -s -L -o wso2is.zip "$file_url" \
 if echo "$response" | grep -q '"error":'; then
   # If there is an error, print the failure message with the error description
   error_description=$(echo "$response" | jq -r '.error_description')
-  echo -e "Failure in downloading Identity Server "$currentVersion" $error_description"
+  echo -e "${RED}Failure in downloading Identity Server "$currentVersion" $error_description${NC}"
 else
   # If there is no error, print the success message
-  echo -e "Success: IS downloaded successfully."
+  echo -e "${GREEN}Success: IS downloaded successfully.${NC}"
 fi
 
 # Unzip IS archive
 unzip -qq *.zip &
 wait $!
-echo "Unzipped  downloaded IS zip"
+echo "${GREEN}==> Unzipped  downloaded IS zip${NC}"
 
 ls -a
-
