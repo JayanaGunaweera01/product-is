7ecb99ee35633cd40adfa327095b87a3f49d5809
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-09-20 13:50:43 +0530

    Update download-GA-pack.sh

--- .github/migration-tester/migration-automation/download-GA-pack.sh
+++ .github/migration-tester/migration-automation/download-GA-pack.sh
@@ -8,6 +8,7 @@ NC='\033[0m'           # reset color
 # Get the value of the inputs
 os=$2
 downloadingISVersion=$3
+startServer=$4
 
 # Setup file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
@@ -16,11 +17,27 @@ if [ "$os" = "ubuntu-latest" ]; then
   . ./env.sh
   echo "${GREEN}==> Env file for Ubuntu sourced successfully${NC}"
 
+  if [ "$startServer" = "current" ]; then
+    cd "$IS_HOME_OLD"
+    echo "${GREEN}==> Navigated to newly created directory to download GA pack $downloadingISVersion ${NC}"
+  elif [ "$startServer" = "migrating" ]; then
+    cd "$IS_HOME_NEW"
+    echo "${GREEN}==> Navigated to newly created directory to download GA pack $downloadingISVersion ${NC}"
+  fi
+
 elif [ "$os" = "macos-latest" ]; then
   cd "/Users/runner/work/product-is/product-is/.github/migration-tester/migration-automation"
   chmod +x env.sh
   source ./env.sh
   echo "${GREEN}==> Env file for Mac sourced successfully${NC}"
+
+  if [ "$startServer" = "current" ]; then
+    cd "$IS_HOME_OLD_MAC"
+    echo "${GREEN}==> Navigated to newly created directory to download GA pack $downloadingISVersion ${NC}"
+  elif [ "$startServer" = "migrating" ]; then
+    cd "$IS_HOME_NEW_MAC"
+    echo "${GREEN}==> Navigated to newly created directory to download GA pack $downloadingISVersion ${NC}"
+  fi
 fi
 
 # Initialize file_id variable
@@ -64,16 +81,15 @@ base64stream() {
     base64 | tr '/+' '_-' | tr -d '=\n'
 }
 
-keyJsonFile=$1
+keyJsonFile=$(echo "$MIGRATION_JSON_ENCODED" | base64 -d)
 scope="https://www.googleapis.com/auth/drive.readonly"
 valid_for_sec="${3:-3600}"
-valid_for_sec_numeric=$(echo "$valid_for_sec" | tr -d -c '[:digit:]')
-private_key=$(jq -r .private_key "$keyJsonFile")
-sa_email=$(jq -r .client_email "$keyJsonFile") || { echo "Error extracting client_email from the JSON file."; exit 1; }
+private_key=$(jq -r .private_key <<< "$keyJsonFile")
+sa_email=$(jq -r .client_email <<< "$keyJsonFile") || { echo "Error extracting client_email from the JSON file."; exit 1; }
 
 header='{"alg":"RS256","typ":"JWT"}'
 # Calculate the expiration time as 'valid_for_sec' seconds from now
-exp=$(($(date +%s) + 60))  # Set the token to expire in 60 seconds
+exp=$(($(date +%s) + 60))
 # The issued at time should be the current time
 iat=$(date +%s)
 claim=$(cat <<EOF | jq -c
@@ -90,8 +106,6 @@ EOF
 request_body="$(base64var "$header").$(base64var "$claim")"
 signature=$(echo -n "$request_body" | openssl dgst -sha256 -sign <(echo "$private_key") -binary | base64stream)
 
-echo "$request_body.$signature"
-
 jwt_token=$(printf "%s.%s" "$request_body" "$signature")
 
 # Manually construct the POST data for token request
@@ -99,10 +113,7 @@ data="grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=$jwt_toke
 
 # Make the token request to get the access token
 token_response=$(curl -s -X POST -d "$data" "https://www.googleapis.com/oauth2/v4/token")
-
-# Extract the access token from the response
 access_token=$(echo "$token_response" | jq -r .access_token)
-echo " test: $access_token"
 
 # Use the file_id variable in downloading the IS zip
 echo "file_id: $file_id"
@@ -120,7 +131,7 @@ if echo "$response" | grep -q '"error":'; then
   # If there is an error, print the failure message with the error description
   error_description=$(echo "$response" | jq -r '.error_description')
   echo -e "${RED}==> Failure in downloading the GA Pack $downloadingISVersion. $error_description.${NC}"
-  
+
 else
   # If there is no error, print the success message
   echo -e "${GREEN}==> Success: GA Pack $downloadingISVersion downloaded successfully.${NC}"
